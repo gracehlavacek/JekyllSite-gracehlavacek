8c983b5d1b3b81a42614fe0b26258bd1e69435a4
Stedman Halliday <stedman.rh@gmail.com>
2021-04-08 02:57:23 +0000

    theme installed

--- main.sh
+++ main.sh
@@ -1,6 +1,7 @@
 echo Please Wait....
+mkdir ../.gem
 cp -r gemBINS ../.gem
 export PATH=/home/runner/.gem/ruby/2.5.0/bin:$PATH
 jekyll -v
-cd site
-jekyll serve -P 8080 --host=0.0.0.0
+cd drava
+JEKYLL_ENV=replit jekyll serve -P 8080 --host=0.0.0.0
