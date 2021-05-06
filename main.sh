d98eaccdfe5ae939bfb3d7bfccd61b55f5a3bd4e
gracehlavacek <>
2021-05-06 02:16:01 +0000

    docs resubmit

--- main.sh
+++ main.sh
@@ -4,5 +4,5 @@ cp -r gemBINS/* ../.gem
 chmod +x /home/runner/.gem/ruby/2.5.0/bin/*
 export PATH=/home/runner/.gem/ruby/2.5.0/bin:$PATH
 jekyll -v
-cd docs
+cd site
 JEKYLL_ENV=replit jekyll serve -P 8080 --host=0.0.0.0
